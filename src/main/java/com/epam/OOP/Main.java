52c100aa16d564bd62b7d678c4a62720b38ff040
kate <katekw20@gmail.com>
2023-06-28 14:41:29 +0300

    first commit

---
+++ src/main/java/com/epam/OOP/Main.java
@@ -0,0 +1,11 @@
+package com.epam.OOP;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        Dog dog = new Dog();
+        System.out.println(bird.getDescription());
+        System.out.println(dog.getDescription());
+    }
+}
